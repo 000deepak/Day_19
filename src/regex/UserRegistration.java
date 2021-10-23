654b6c3d6633196eb7443387247de7697f7913b5
000deepak <90568067+000deepak@users.noreply.github.com>
2021-10-23 16:20:34 +0530

    Update UserRegistration.java

--- src/regex/UserRegistration.java
+++ src/regex/UserRegistration.java
@@ -1,30 +1,21 @@
 package regex;
 
-
-
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 public class UserRegistration {
 
-
     public static void main(String[] args) {
-
         Scanner sc=new Scanner(System.in);
-
-        String fname_match = "[A-Z]{1}[a-zA-Z]{2,}";			//String pattern to be detected
-
-
+        String fname_match = "[A-Z]{1}[a-zA-Z]{2,}";		                 	//String pattern to be detected
         String firstname;
-
-
+        
         //Checking for matches
-
         while(true) {
             //UC1_Entering firstname
             System.out.print("Please enter your First Name: ");
             firstname = sc.nextLine();
-
             boolean check=firstname.matches(fname_match);							//Condition check
             if(check) {
                 break;
